74493dfcc4d43b1e62ae2bb423f987f410762582
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2021-10-30 00:04:13 +0700

    Update JS on search bar but still currently not functional. Added screenshot of the main page of the web

--- main.js
+++ main.js
@@ -1,40 +1,8 @@
-// // Get modal element
-// var modal = document.getElementById('area-modal');
-// // Get open modal container
-// var modal_movie = document.getElementById('movieItem-modal');
-// // Get close button
-// var close_btn = document.getElementsByClassName('close-btn')[0];
-
-// // Listen for open click
-// modal_movie.addEventListener('click', open_modal);
-// // Listen for close click
-// close_btn.addEventListener('click', close_modal);
-// // Listen for outside click
-// window.addEventListener('click', outside_click);
-
-// // Function to open modal
-// function open_modal(){
-//     modal.style.display = 'block';
-// }
-
-// // Function to close modal
-// function close_modal(){
-//     modal.style.display = 'none';
-// }
-
-// // Function to close modal if outside is click
-// function outside_click(e){
-//     if(e.target === modal){
-//         modal.style.display = 'none';
-//     }
-// }
-
-
 // Get the modal
 var modal_parent = document.getElementsByClassName("all_movies");
 
 // Get open modal container
-var open_modal = document.getElementsByClassName("modalTrigger-movieItem");
+var open_modal = document.getElementsByClassName("movie-item");
 
 // Get close button
 var close_modal = document.getElementsByClassName("close-btn");
@@ -52,13 +20,13 @@ function setDataIndex() {
 for (i = 0; i < open_modal.length; i++)
 {
     open_modal[i].onclick = function() {
-        var ElementIndex = this.getAttribute('data-index');
+        var ElementIndex = this.getAttribute("data-index");
         modal_parent[ElementIndex].style.display = "block";
     }
     
     // When user click close button
     close_modal[i].onclick = function() {
-        var ElementIndex = this.getAttribute('data-index');
+        var ElementIndex = this.getAttribute("data-index");
         modal_parent[ElementIndex].style.display = "none";
     }
 }
@@ -72,3 +40,38 @@ window.onclick = function(event) {
         modal_parent[event.target.getAttribute("data-index")].style.display = "none";
     }
 }
+
+
+// Search Bar (Cyrrently Not Functional)
+
+// function search_movie() {
+//     console.log(123)
+//     let input = document.getElementById("searchbar").value
+//     input = input.toLowerCase();
+//     let x = document.getElementsByClassName("movie-item");
+
+//     for(i = 0; i < x.length; i++) {
+//         if (!x[i].innerHTML.toLocaleLowerCase().includes(input)) {
+//             x[i].style.display = "none";
+//         }
+//         else {
+//             x[i].style.display = "list-item";
+//         }
+//     }
+// }
+
+// // JavaScript code
+// function search_animal() {
+// 	let input = document.getElementById('searchbar').value
+// 	input=input.toLowerCase();
+// 	let x = document.getElementsByClassName('animals');
+	
+// 	for (i = 0; i < x.length; i++) {
+// 		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+// 			x[i].style.display="none";
+// 		}
+// 		else {
+// 			x[i].style.display="list-item";				
+// 		}
+// 	}
+// }
